2728a9f830721db5d1d515c4821bbc49b1c249f0
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2023-09-11 16:17:46 +0100

    using native queries

--- src/main/java/com/tsola2002/springMysql/Repository/StudentRepository.java
+++ src/main/java/com/tsola2002/springMysql/Repository/StudentRepository.java
@@ -1,12 +1,46 @@
 package com.tsola2002.springMysql.Repository;
 
 import com.tsola2002.springMysql.Entity.Student;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
   // BUILDING OUT A CUSTOM QUERY
+  @Query("SELECT s FROM Student s WHERE s.email = ?1")
   Optional<Student> findStudentByEmail(String email);
+
+
+  List<Student> findStudentsByFirstNameEqualsAndAgeEquals(String firstName, Integer age);
+
+  @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
+  List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThan(String firstName, Integer age);
+
+  //NATIVE MYSQL QUERY
+//  @Query(
+//      value = "SELECT * FROM student WHERE firstName = ?1 AND age >= ?2",
+//      nativeQuery = true)
+//  List<Student> selectStudentWhereFirstNameAndAgeIsGreaterThanOrEqualNative(String firstName, Integer age);
+
+  // NATIVE QUERY USING NAMED PARAMETERS
+  @Query(
+      value = "SELECT * FROM tbl_student WHERE first_name = :firstName AND age >= :age",
+      nativeQuery = true)
+  List<Student> selectStudentWhereFirstNameAndAgeIsGreaterThanOrEqualNative(
+      @Param("firstName")
+      String firstName,
+      @Param("age")
+      Integer age);
+
+  //DELETING RECORDS USING @MODIFYING
+  @Transactional
+  @Modifying
+  @Query("DELETE FROM Student u WHERE u.id = ?1")
+  int deleteStudentById(Long id);
 
 }
--- src/main/java/com/tsola2002/springMysql/SpringMysqlApplication.java
+++ src/main/java/com/tsola2002/springMysql/SpringMysqlApplication.java
@@ -19,18 +19,44 @@ public class SpringMysqlApplication {
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
 		return args -> {
 			Student maria = new Student("Maria", "Jones", "Maria.Jones@edu", 21);
+			Student maria2 = new Student("Maria", "Jones", "Maria.Jones@edu", 25);
 			Student ahmed = new Student("Ahmed", "Ali", "Ahmed.Ali@edu", 18);
 			//studentRepository.save(maria);
 
 
-
 			System.out.println("SAVING MARIA & AHMED");
-			studentRepository.saveAll(List.of(maria, ahmed));
+			studentRepository.saveAll(List.of(maria, maria2, ahmed));
 
-			System.out.println("USING CUSTOM QUERY");
-			studentRepository
-					.findStudentByEmail("Ahmed.Ali@edu")
-					.ifPresentOrElse(System.out::println, () -> System.out.println("Student with email does not exist"));
+			System.out.println("DELETING MARIA WITH ID 2");
+			System.out.println(studentRepository.deleteStudentById(3L));
+
+//			System.out.println("USING NATIVE QUERIES & NAMED PARAMETERS");
+//			studentRepository.selectStudentWhereFirstNameAndAgeIsGreaterThanOrEqualNative(
+//					"Maria2",
+//					25
+//			).forEach(System.out::println);
+
+//			System.out.println("USING NATIVE QUERIES");
+//			studentRepository.selectStudentWhereFirstNameAndAgeIsGreaterThanOrEqualNative(
+//					"Maria2",
+//					25
+//			).forEach(System.out::println);
+
+//			System.out.println("USING CUSTOM QUERY2");
+//			studentRepository.findStudentsByFirstNameEqualsAndAgeEquals(
+//					"Maria2",
+//					25
+//			).forEach(System.out::println);
+
+//			studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThan(
+//					"Maria",
+//					19
+//			).forEach(System.out::println);
+
+//			System.out.println("USING CUSTOM QUERY");
+//			studentRepository
+//					.findStudentByEmail("Ahmed.Ali@edu")
+//					.ifPresentOrElse(System.out::println, () -> System.out.println("Student with email does not exist"));
 
 //			System.out.println("NUMBER OF STUDENTS: ");
 //			System.out.println(studentRepository.count());
@@ -40,20 +66,22 @@ public class SpringMysqlApplication {
 //					.ifPresentOrElse(
 //							System.out::println,
 //							() -> System.out.println("Student with ID 2 not found"));
-//
+
+
 //			studentRepository
 //					.findById(3L)
 //					.ifPresentOrElse(
 //							System.out::println,
 //							() -> System.out.println("Student with ID 3 not found"));
-//
+
+
 //					System.out.println("SELECT ALL STUDENTS");
 //					List<Student> students = studentRepository.findAll();
 //					students.forEach(System.out::println);
-//
+
 //					System.out.println("DELETE MARIA");
 //					studentRepository.deleteById(1L);
-//
+
 //					System.out.println("NUMBER OF STUDENTS AFTER DELETE");
 //					System.out.println(studentRepository.count());
 
